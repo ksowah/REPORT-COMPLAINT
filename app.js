20714185e5c382e87d4ed387b8e145f391c04437
ksowah <sowahkelvin640@gmail.com>
2022-04-14 19:08:54 +0000

    add js logic

--- app.js
+++ app.js
@@ -2,23 +2,60 @@ const name = document.getElementById("name");
 const student_id = document.getElementById("student_id");
 const room_number = document.getElementById("room_number");
 const message = document.getElementById("message");
+const error = document.getElementById("error");
+const alertMessage = document.getElementById("alert");
+const alert_head = document.getElementById("alert_head");
 
 const validate = () => {
+
+	let valid = null
+
 	if (name.value === "") {
-		alert("Please enter your name");
-		return false;
+		valid = false
+		error.innerText = "Please enter your name";
+		alertMessage.style.visibility = "visible";
+		alert_head.innerText = "ERROR: "
 	}
 
-	if (student_id.value === "" || isNaN(student_id.value) || student_id.value.length !== 8) {
-		alert("Please enter a valid student id");
-		return false;
+	else if (student_id.value === "" || isNaN(student_id.value) || student_id.value.length !== 8) {
+		valid = false
+		error.innerText = "Please enter a valid student id";
+		alertMessage.style.visibility = "visible";
+		alert_head.innerText = "ERROR: "
 	}
 
-    if(room_number.value === "" || isNaN(room_number.value) || room_number.value.length !== 4) {
-        alert("Please enter a room number");
+    else if(room_number.value === "" || isNaN(room_number.value) || room_number.value.length > 4) {
+		valid = false
+		error.innerText = "Please enter a valid room number";
+		alertMessage.style.visibility = "visible";
+		alert_head.innerText = "ERROR: "
     }
 
-    if(message.value === ""){
-        alert("Please enter your report message");
+    else if(message.value === ""){
+		valid = false
+		error.innerText = "Please enter your report message";
+		alertMessage.style.visibility = "visible";
+		alert_head.innerText = "ERROR: "
     }
+
+	else {
+		valid = true
+		alert_head.innerText = "CONGRATULATIONS! "
+		error.innerText = "Your report has been sent. Thank you!";
+	}
+
+	if(valid){
+		alertMessage.style.visibility = "visible";
+		alertMessage.style.background = "#dff0d8";
+		alertMessage.style.borderColor = "#d6e9c6";
+		alertMessage.style.color = "#3c763d";
+	}
+
+	if(valid === false){
+		alertMessage.style.visibility = "visible";
+		alertMessage.style.background = "#f2dede";
+		alertMessage.style.borderColor = "#ebccd1";
+		alertMessage.style.color = "#a94442";
+	}
+
 };
