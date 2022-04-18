e658a0180e76057f7f37b205fd21eed48d27c0fb
ksowah <sowahkelvin640@gmail.com>
2022-04-18 16:53:18 +0000

    make some additions

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ let savedReports = []
 
 const storedReports = JSON.parse(localStorage.getItem("savedReports"));
 
+if(storedReports) {
+	savedReports = storedReports;
+}
+
 const validate = () => {
 
 	let valid = null
@@ -67,7 +71,7 @@ const validate = () => {
 };
 
 const storeReports = () => {
-	const report = {
+	let report = {
 		name: name.value,
 		student_id: student_id.value,
 		room_number: room_number.value,
@@ -75,7 +79,8 @@ const storeReports = () => {
 	}
 
 	savedReports.push(report);
+	report = {}
 	localStorage.setItem("savedReports", JSON.stringify(savedReports));
-
-	console.log(savedReports);
 }
+
+console.log(storedReports);
